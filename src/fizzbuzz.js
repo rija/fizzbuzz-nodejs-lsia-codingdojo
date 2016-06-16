879460a47613cc70ea7f4e93d5ff176f2af6e351
Rija Ménagé <git@rijam.sent.as>
2016-06-16 14:43:22 +0800

    completed another solo run

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,30 +1,36 @@
 'use strict'
 
 module.exports = {
-  check_number: (number) => {
-
-    if ( number % 3 === 0 && number % 5 === 0 ) {
-      return 'FizzBuzz'
-    }
-
-    if ( number % 5 === 0 ) {
-      return 'Buzz'
-    }
-
-    if ( number % 3 === 0 ) {
-      return 'Fizz'
-    }
-
-    if ( number.toString().search('5') > -1 ) {
-      return 'Buzz'
-    }
-
-    if ( number.toString().search('3') > -1 ) {
-      return 'Fizz'
-    }
+  check_number: check_number
+}
 
 
+function check_number(number) {
 
-    return number
+  if ( number % 3 === 0 && number % 5 === 0 ) {
+    return 'FizzBuzz'
   }
+
+  if ( number % 5 === 0 ) {
+    return 'Buzz'
+  }
+
+  if ( number % 3 === 0 ) {
+    return 'Fizz'
+  }
+
+  if ( number.toString().search('5') > -1 ) {
+    return 'Buzz'
+  }
+
+  if ( number.toString().search('3') > -1 ) {
+    return 'Fizz'
+  }
+
+  return number
+}
+
+
+for ( var i=1 ; i<=100 ; i++) {
+  console.log( check_number(i) )
 }
